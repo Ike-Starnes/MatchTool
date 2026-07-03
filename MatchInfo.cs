87ba5e499c18b2cb916fad4a53fe5810f81c4fc2
Ike Starnes <ikestarnes@live.com>
2026-07-03 09:59:33 -0400

    Cleanup using statements

--- MatchInfo.cs
+++ MatchInfo.cs
@@ -1,12 +1,4 @@
 ﻿using HtmlAgilityPack;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MatchTool
 {
--- MatchOptions.cs
+++ MatchOptions.cs
@@ -1,11 +1,4 @@
-﻿using CommandLine;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MatchTool
+﻿namespace MatchTool
 {
    internal class MatchOptions
    {
